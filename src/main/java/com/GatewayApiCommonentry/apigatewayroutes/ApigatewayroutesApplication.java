42c7fd9f8be4bd3fbbe2d78740705147d7a5424a
Monudhakad1 <monudhakad055@gmail.com>
2026-04-14 10:36:04 +0530

    Api gateway now working

---
+++ src/main/java/com/GatewayApiCommonentry/apigatewayroutes/ApigatewayroutesApplication.java
@@ -0,0 +1,15 @@
+package com.GatewayApiCommonentry.apigatewayroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApigatewayroutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayroutesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GatewayApiCommonentry/apigatewayroutes/ApigatewayroutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GatewayApiCommonentry.apigatewayroutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayroutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
